ce593e406b578ea553fe2a7b5af208e281a5676c
Tom Willmott <tom.willmott@hotmail.com>
2019-01-06 20:08:03 +0000

    Updating extruder stepper motor steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 3250, 1400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 3250, 1440 }
 
 /**
  * Default Max Feed Rate (mm/s)
